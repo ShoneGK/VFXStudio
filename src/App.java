1532f0a6a31beee5f7c3261bf8826062683021e3
Shone Kaithathara <shonekaithathara@gmail.com>
2023-02-18 14:13:04 -0500

    added base theme

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
+
 import java.awt.BorderLayout;
+import java.awt.Color;
 
 import org.fife.ui.rtextarea.*;
 import org.fife.ui.rsyntaxtextarea.*;
@@ -9,13 +11,15 @@ public class App extends JFrame {
     public App() {
 
         JPanel cp = new JPanel(new BorderLayout());
-
+        
         RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
+        
+        changeStyleProgrammatically(textArea);
+
         textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
         textArea.setCodeFoldingEnabled(true);
-        RTextScrollPane sp = new RTextScrollPane(textArea);
+        RTextScrollPane sp = new RTextScrollPane(textArea, true, new Color(133, 133, 133));
         cp.add(sp);
-
         setContentPane(cp);
         setTitle("Text Editor Demo");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -29,4 +33,19 @@ public class App extends JFrame {
         SwingUtilities.invokeLater(() -> new App().setVisible(true));
     }
 
+    // change style programmatically
+    private static void changeStyleProgrammatically(RSyntaxTextArea textArea) {
+        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_PYTHON);
+        textArea.setCodeFoldingEnabled(true);
+        textArea.setAntiAliasingEnabled(true);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setCaretColor(Color.BLACK);
+        textArea.setForeground(Color.WHITE);
+        textArea.setMatchedBracketBGColor(new Color(40, 40, 40));
+        textArea.setMatchedBracketBorderColor(Color.LIGHT_GRAY);
+        //textArea.setCurrentLineHighlightColor(new Color(40, 40, 40));
+        textArea.setCurrentLineHighlightColor(new Color(30, 30, 30));
+        textArea.setBackground(new Color(30, 30, 30));
+    }
 }
