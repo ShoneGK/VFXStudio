111bf79c59ce97f071c2cbcfd07bcc457053d903
Shone Kaithathara <shonekaithathara@gmail.com>
2023-02-18 11:22:42 -0500

    basic notepad

---
+++ src/App.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.BorderLayout;
+
+import org.fife.ui.rtextarea.*;
+import org.fife.ui.rsyntaxtextarea.*;
+
+public class App extends JFrame {
+
+    public App() {
+
+        JPanel cp = new JPanel(new BorderLayout());
+
+        RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
+        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+        textArea.setCodeFoldingEnabled(true);
+        RTextScrollPane sp = new RTextScrollPane(textArea);
+        cp.add(sp);
+
+        setContentPane(cp);
+        setTitle("Text Editor Demo");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+
+    }
+
+    public static void main(String[] args) {
+        // Start all Swing applications on the EDT.
+        SwingUtilities.invokeLater(() -> new App().setVisible(true));
+    }
+
+}
